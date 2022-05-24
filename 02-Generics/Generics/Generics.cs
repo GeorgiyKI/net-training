5c595cc80bc306a75abfedf46fb947f79941953e
GeorgiyKI <balerka228@gmail.com>
2022-05-24 12:46:58 +0300

    Generics, fixed after first review

--- 02-Generics/Generics/Generics.cs
+++ 02-Generics/Generics/Generics.cs
@@ -2,112 +2,90 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
-using System.Text;
 
-namespace Task.Generics {
+namespace Task.Generics
+{
 
-	public static class ListConverter {
+    public static class ListConverter
+    {
 
-		private static char ListSeparator = ',';  
+        private static char ListSeparator = ',';
 
-		public static string ConvertToString<T>(this IEnumerable<T> list) {
-			if (list == null)
-				throw new ArgumentNullException("List is null");
-
-			StringBuilder stringBuilder = new StringBuilder();
-			foreach (var item in list)
-				stringBuilder.Append(item.ToString() + ListSeparator);
-
-			return stringBuilder.ToString().Remove(stringBuilder.Length - 1);
-		}
-
-		public static IEnumerable<T> ConvertToList<T>(this string list) {
-			if (list == null)
-				throw new ArgumentNullException("List is null");
-
-			var result = new List<T>();
-			foreach (var item in list.Split(ListSeparator))
-				result.Add((T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(item));
-
-			return result;
-		}
-	}
-
-	public static class ArrayExtentions {
-		public static void SwapArrayElements<T>(this T[] array, int index1, int index2) {
-			(array[index1], array[index2]) = (array[index2], array[index1]);
-		}
-
-		public static void SortTupleArray<T1, T2, T3>(this Tuple<T1, T2, T3>[] array, int sortedColumn, bool ascending)
-		where T1 : IComparable
-		where T2 : IComparable
-		where T3 : IComparable
+        public static string ConvertToString<T>(this IEnumerable<T> list)
         {
-            if (sortedColumn == 0)
-                Array.Sort(array, (x, y) => y.Item1.CompareTo(x.Item1));
-            else if (sortedColumn == 1)
-                Array.Sort(array, (x, y) => y.Item2.CompareTo(x.Item2));
-            else if (sortedColumn == 2)
-                Array.Sort(array, (x, y) => y.Item3.CompareTo(x.Item3));
-            else
-                throw new IndexOutOfRangeException("Sorted column is less than 0 or more than 2");
+            return string.Join(ListSeparator.ToString(), list);
+        }
 
-            if (ascending)
-				Array.Reverse(array);
-	}
-}
+        public static IEnumerable<T> ConvertToList<T>(this string list)
+        {
+            var desctiptor = TypeDescriptor.GetConverter(typeof(T));
+            foreach (var item in list.Split(ListSeparator))
+            {
+                yield return (T)desctiptor.ConvertFrom(item);
+            }
+        }
+    }
 
-	public static class Singleton<T> where T : new()
-	{
-		static T _insance;
-		private static readonly object close = new object();
-		public static T Instance
-		{
-			get
-			{
-				lock (close)
-				{
-					if (_insance == null)
-						_insance = new T();
-					return _insance;
-				}
-			}
-		}
-	}
+    public static class ArrayExtentions
+    {
+        public static void SwapArrayElements<T>(this T[] array, int index1, int index2)
+        {
+            (array[index1], array[index2]) = (array[index2], array[index1]);
+        }
 
-	public static class FunctionExtentions {
-		public static T TimeoutSafeInvoke<T>(this Func<T> function) 
-		{
-			int countOfRestarts = 0;
-			while (countOfRestarts < 3)
-			{
-				try
-				{
-					return function();
-				}
-				catch (Exception ex)
-				{
-					Trace.WriteLine(ex.ToString());
-					countOfRestarts++;
-				}
-			}
+        public static void SortTupleArray<T1, T2, T3>(this Tuple<T1, T2, T3>[] array, int sortedColumn, bool ascending)
+        where T1 : IComparable
+        where T2 : IComparable
+        where T3 : IComparable
+        {
+            if (sortedColumn >= array.Length || sortedColumn < 0)
+                throw new IndexOutOfRangeException();
 
-			throw new WebException("The operation has timed out", WebExceptionStatus.Timeout);
-		}
+            Array.Sort(array, (x, y) =>
+            {
+                dynamic a = y.GetType().GetProperty($"Item{sortedColumn + 1}").GetValue(y);
+                dynamic b = x.GetType().GetProperty($"Item{sortedColumn + 1}").GetValue(x);
+                return ascending ? -a.CompareTo(b) : a.CompareTo(b);
+            });
+        }
+    }
 
-		public static Predicate<T> CombinePredicates<T>(Predicate<T>[] predicates) 
-		{
-			return (T x) =>
-			{
-				foreach (var predicate in predicates)
-				{
-					if (!predicate(x))
-						return false;
-				}
+    public static class Singleton<T> where T : new()
+    {
+        private static readonly Lazy<T> lazy = new Lazy<T>(() => new T());
+        public static T Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
 
-				return true;
-			};
-		}
-	}
+    public static class FunctionExtentions
+    {
+        public static T TimeoutSafeInvoke<T>(this Func<T> function)
+        {
+            for (int attempts = 0; attempts < 3; attempts++)
+            {
+                try
+                {
+                    return function();
+                }
+                catch (WebException ex)
+                {
+                    Trace.WriteLine(ex.ToString());
+                }
+            }
+
+            throw new WebException("The operation has timed out", WebExceptionStatus.Timeout);
+        }
+
+        public static Predicate<T> CombinePredicates<T>(Predicate<T>[] predicates)
+        {
+            return (T x) => predicates.All(predicate => predicate.Invoke(x));
+        }
+    }
 }
